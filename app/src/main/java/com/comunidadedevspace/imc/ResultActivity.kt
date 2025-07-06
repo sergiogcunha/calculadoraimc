dc41161ec848993763cb50a20c7e18c27a74b54b
sergiogcunha <sergiogcunha@gmail.com>
2025-07-06 20:43:56 -0300

    melhorando experiencia usuário

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -22,20 +23,24 @@ class ResultActivity : AppCompatActivity() {
 
         tvResult.text = result.toString()
 
-        val classificacao: String = if (result <= 18.5f){
-            "MAGREZA"
+        val (classificacao : String,colorRes) = if (result <= 18.5f){
+            "MAGREZA" to R.color.imc_magreza
         } else if (result > 18.5f && result <= 24.9f){
-            "NORMAL"
+            "NORMAL" to R.color.imc_normal
         } else if (result > 25f && result <=29.9f){
-            "SOBREPESO"
+            "SOBREPESO" to R.color.imc_sobrepeso
         } else if (result > 30 && result <= 39.9f){
-            "OBESIDADE"
+            "OBESIDADE" to R.color.imc_obesidade
         } else {
-            "OBESIDADE GRAVE"
+            "OBESIDADE GRAVE" to R.color.imc_obesidade_grave
         }
 
         tvClassificacao.text = classificacao
+        tvClassificacao.setTextColor(ContextCompat.getColor(this,colorRes))
         //println("Sergio Resultado " + result)
+
+
+
 
     }
 }
